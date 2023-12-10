492168b3707661365ab2b8bd9ba185f3499a5861
shcherbana <shcherba.pn@ucu.edu.ua>
2023-12-10 22:00:16 +0200

    Tray add + ATM + Main

---
+++ src/main/java/org/example/task1/Tray200.java
@@ -0,0 +1,7 @@
+package org.example.task1;
+
+public class Tray200 extends Tray {
+    public Tray200() {
+        super(200);
+    }
+}
---
+++ src/main/java/org/example/task1/Tray500.java
@@ -0,0 +1,7 @@
+package org.example.task1;
+
+public class Tray500 extends Tray {
+    public Tray500() {
+        super(500);
+    }
+}
